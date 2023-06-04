7e59fe111afe6d3f9aefce39fffc1ba5a15a4187
LuigiBrancati <luigi.brancati@flowe.com>
2023-06-04 16:09:35 +0200

    Added linter comments

--- atmospheric_explorer/loggers.py
+++ atmospheric_explorer/loggers.py
@@ -7,4 +7,6 @@ import os
 
 _logfile_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logging.ini")
 logging.config.fileConfig(_logfile_path)
+# pylint: disable=unused-argument
+# ruff: noqa: F401
 _main_logger = logging.getLogger("main")
