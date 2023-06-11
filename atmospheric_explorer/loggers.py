b427a71b9e8f7f9536783067f330fb0b69bb39ad
LuigiBrancati <luigi.brancati@flowe.com>
2023-06-11 11:39:37 +0200

    Replaced logging file with dict

--- atmospheric_explorer/loggers.py
+++ atmospheric_explorer/loggers.py
@@ -5,8 +5,38 @@ import logging
 import logging.config
 import os
 
-_logfile_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logging.ini")
-logging.config.fileConfig(_logfile_path)
+from .utils import get_local_folder
+
+_LOGGING_CONFIG = {
+    "version": 1,
+    "loggers": {
+        "root": {"handlers": ["console"], "level": "ERROR"},
+        "main": {
+            "handlers": ["console", "rotatingfile"],
+            "level": "DEBUG",
+            "propagate": 0,
+            "qualname": "main",
+        },
+    },
+    "handlers": {
+        "console": {"class": "logging.StreamHandler", "formatter": "simple"},
+        "rotatingfile": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "formatter": "verbose",
+            "maxBytes": 1024,
+            "backupCount": 3,
+            "filename": os.path.join(get_local_folder(), "logconfig.log"),
+        },
+    },
+    "formatters": {
+        "simple": {"format": "%(levelname)s %(message)s"},
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s"  # noqa: E501
+        },
+    },
+}
+
+logging.config.dictConfig(_LOGGING_CONFIG)
 # pylint: disable=unused-argument
 # ruff: noqa: F401
 _main_logger = logging.getLogger("main")
