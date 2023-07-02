9e320d802e5611b7d0a0e5481a54028bcdac2422
LuigiBrancati <luigi.brancati@flowe.com>
2023-07-02 16:58:52 +0200

    Fix changes

--- atmospheric_explorer/ui/app.py
+++ atmospheric_explorer/ui/app.py
@@ -1,8 +1,14 @@
 """\
 Module for building the UI
 """
+
+import pandas as pd
 import streamlit as st
 
+from atmospheric_explorer.plotting_apis import (
+    eac4_anomalies_plot,
+    ghg_surface_satellite_yearly_plot,
+)
 from atmospheric_explorer.ui.utils import build_sidebar, show_folium_map, update_session
 
 st.set_page_config(
@@ -32,3 +38,37 @@ out_event = show_folium_map()
 update_session(out_event)
 PROGRESS_BAR.progress(1.0, "Done")
 PROGRESS_BAR.empty()
+with st.expander("Plots"):
+    start_date = st.session_state.get("start_date")
+    end_date = st.session_state.get("end_date")
+    countries = (
+        st.session_state.get("selected_state")
+        if isinstance(st.session_state.get("selected_state"), list)
+        else [st.session_state.get("selected_state")]
+    )
+    years = [y.strftime("%Y") for y in pd.date_range(start_date, end_date, freq="YS")]
+    months = list(
+        {m.strftime("%m") for m in pd.date_range(start_date, end_date, freq="MS")}
+    )
+    with st.container():
+        col1, col2 = st.columns(2)
+        col1.plotly_chart(
+            ghg_surface_satellite_yearly_plot(
+                "carbon_dioxide",
+                countries,
+                years,
+                months,
+                "Fossil CO2 flux",
+                "flux_foss",
+            )
+        )
+        col2.plotly_chart(
+            eac4_anomalies_plot(
+                "total_column_nitrogen_dioxide",
+                "tcno2",
+                countries,
+                f"{start_date.strftime('%Y-%m-%d')}/{end_date.strftime('%Y-%m-%d')}",
+                "00:00",
+                "total_column_nitrogen_dioxide",
+            )
+        )
