b2525dedeeb4660fc1b4f741f9e5b97d562c5075
LuigiBrancati <luigi.brancati@flowe.com>
2023-07-02 17:07:27 +0200

    Split old and new plotting apis, fixed ghg api

--- atmospheric_explorer/plotting_apis.py
+++ atmospheric_explorer/plotting_apis.py
@@ -3,8 +3,6 @@ APIs for generating dynamic and static plots
 """
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-from enum import Enum
 from math import ceil, log10
 
 import numpy as np
@@ -241,7 +239,6 @@ def ghg_surface_satellite_yearly_plot(
     years: list[str],
     months: list[str],
     title: str,
-    col_num: int = 2,
     var_name: str = "flux_foss",
 ) -> go.Figure:
     """Generate a yearly mean plot with CI for a quantity from the CAMS Global Greenhouse Gas Inversion dataset"""
@@ -256,6 +253,7 @@ def ghg_surface_satellite_yearly_plot(
         .droplevel(axis=1, level=0)
         .reset_index(["admin", "input_observations"])
     )
+    col_num = 2 if len(countries) >= 2 else 1
     return line_with_ci_subplots(
         df_pandas, countries, col_num, da_converted_agg.attrs["units"], title
     )
@@ -415,59 +413,3 @@ def eac4_hovmoeller_levels_plot(
         coloraxis={"colorscale": colorscale, "colorbar": colorbar},
     )
     return fig
-
-
-PlotType = Enum("PlotType", ["time_series", "hovmoeller"])
-
-
-class PlottingInterface(ABC):
-    """Generic interface for all plotting APIs"""
-
-    _plot_type: PlotType
-
-    def __init__(
-        self: PlottingInterface,
-        _data_variable: str,
-        _var_name: str,
-        _time_period: str,
-    ):
-        self.data_variable = _data_variable
-        self.var_name = _var_name
-        self.time_period = _time_period
-
-    @property
-    def data_variable(self: PlottingInterface) -> str:
-        """Data variable name"""
-        return self._data_variable
-
-    @data_variable.setter
-    def data_variable(
-        self: PlottingInterface,
-        data_variable_input: str,
-    ) -> None:
-        self._data_variable = data_variable_input
-
-    @abstractmethod
-    def plot(self: PlottingInterface):
-        """Returns a plot"""
-        raise NotImplementedError("Method not implemented")
-
-
-class TimeSeriesPlotInstance(PlottingInterface):
-    """Time series plot object"""
-
-    _plot_type: PlotType = PlotType.time_series
-
-    # def __init__(
-    #     self: PlottingInterface,
-    #     data_variable: str,
-    #     var_name: str,
-    #     time_period: str,
-    # ):
-    #     super().__init__(data_variable, var_name, time_period)
-
-    def plot(
-        self: TimeSeriesPlotInstance,
-    ) -> go.Figure:
-        fig = px.line()
-        return fig
