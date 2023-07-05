e7d1d334b93c4257d3ba17d58f199c0b3007596f
LuigiBrancati <luigi.brancati@flowe.com>
2023-07-05 20:19:26 +0200

    Added CLI command to run streamlit app

---
+++ atmospheric_explorer/ui/run_streamlit_app.py
@@ -0,0 +1,14 @@
+"""\
+Module for building the UI
+"""
+from pathlib import Path
+
+# pylint: disable=invalid-name
+import click
+from streamlit.web.bootstrap import run
+
+
+@click.command()
+def run_streamlit_app():
+    """Run this app from a simple cli command"""
+    run(f"{Path(__file__).resolve().parent.joinpath('Home.py')}", "", [], {})
