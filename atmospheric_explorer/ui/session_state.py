13eade1bdad473a7bd7be02b93bccf27da7d7ae5
elisaliv <49102376+elisaliv@users.noreply.github.com>
2023-08-16 23:10:53 +0200

    Remove CO mapping from EAC4 variables

--- atmospheric_explorer/ui/session_state.py
+++ atmospheric_explorer/ui/session_state.py
@@ -53,13 +53,11 @@ class GHGSessionStateKeys(Enum):
 eac4_data_variable_var_name_mapping = {
     "total_column_nitrogen_dioxide": "tcno2",
     "total_column_ozone": "gtco3",
-    "carbon_monoxide": "co",
 }
 
 eac4_data_variable_default_plot_title_mapping = {
     "total_column_nitrogen_dioxide": "Total column NO2",
     "total_column_ozone": "Total column O3",
-    "carbon_monoxide": "CO",
 }
 
 eac4_data_variables = list(eac4_data_variable_var_name_mapping.keys())
