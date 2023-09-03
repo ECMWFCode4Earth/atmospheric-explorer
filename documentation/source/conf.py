5af6d22e7eea06e03d57a8c6eec81ac296d27d2d
luigi.brancati <luigi.brancati93@gmail.com>
2023-09-03 23:50:03 +0200

    Generate sphinx config

---
+++ documentation/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Atmospheric Explorer"
+copyright = "2023, Elisa Aliverti, Luigi Brancati, Giulia Fiantanese"
+author = "Elisa Aliverti, Luigi Brancati, Giulia Fiantanese"
+release = "0.1.0.dev0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
