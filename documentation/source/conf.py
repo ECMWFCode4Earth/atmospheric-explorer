8ffd495dd0cddc62ee6a1dbeb327d04f26fcb003
luigi.brancati <luigi.brancati93@gmail.com>
2023-09-06 20:26:39 +0200

    Changed docs theme

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -34,6 +34,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "classic"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
